c2d4c4e8a4a2f5eb7a28c08981196c5b31abe791
Francisco Viana <franciscosantos29@gmail.com>
2024-10-19 21:30:35 -0300

    Estendendo o JpaRepository para customizar o repositório base

---
+++ src/main/java/com/fsv/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.fsv.algafood.infrastructure.repository;
+
+import com.fsv.algafood.domain.repository.CustomJpaRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.util.Optional;
+
+public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements CustomJpaRepository<T, ID> {
+
+    private EntityManager entityManager;
+
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        var jpql = "from " + getDomainClass().getName();
+
+        T entity = entityManager.createQuery(jpql, getDomainClass())
+                .setMaxResults(1)
+                .getSingleResult();
+
+        return Optional.ofNullable(entity);
+    }
+}
---
+++ src/main/java/com/fsv/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,69 @@
+package com.fsv.algafood.infrastructure.repository;
+
+import com.fsv.algafood.domain.model.Restaurante;
+import com.fsv.algafood.domain.repository.RestauranteRepository;
+import com.fsv.algafood.domain.repository.RestauranteRepositoryQueries;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.fsv.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.fsv.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Lazy
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Override
+    public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Restaurante> criteriaQuery = builder.createQuery(Restaurante.class);
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class); // from Restaurante
+
+        var predicates = new ArrayList<>();
+
+        if (StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+
+        if (taxaFreteInicial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+
+        if (taxaFreteFinal != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+
+        TypedQuery<Restaurante> query = entityManager.createQuery(criteriaQuery);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Restaurante> findComFreteGratis(String nome) {
+        return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
+    }
+}
